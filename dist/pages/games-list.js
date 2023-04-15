e131a74ee06a5225888eeccc6f3e3e04ae35d6b9
WebGL3D <WebGL3D@users.noreply.github.com>
2023-04-15 09:54:24 -0700

    2.4.61
    https://github.com/roblox-plus/extension/pull/64

--- dist/pages/games-list.js
+++ dist/pages/games-list.js
@@ -62,6 +62,49 @@ var __webpack_exports__ = {};
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _css_pages_games_list_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../../css/pages/games-list.scss */ "./src/css/pages/games-list.scss");
 
+const rankAttribute = 'rplus-rank-indicator';
+const getTileRank = (gameTile, index) => {
+    if (gameTile.previousElementSibling instanceof HTMLElement) {
+        const indicator = Number(gameTile.previousElementSibling.getAttribute(rankAttribute) || NaN);
+        if (!isNaN(indicator)) {
+            return indicator + 1;
+        }
+    }
+    return index + 1;
+};
+const addRankIndicator = (gameTile, index) => {
+    const rank = getTileRank(gameTile, index);
+    gameTile.setAttribute(rankAttribute, `${rank}`);
+    const thumbnail = gameTile.querySelector('.game-card-link');
+    if (thumbnail instanceof HTMLElement) {
+        const rankContainer = document.createElement('div');
+        rankContainer.setAttribute('class', 'rplus-rank-indicator');
+        const rankIndicator = document.createElement('b');
+        rankIndicator.innerText = `#${rank.toLocaleString()}`;
+        rankContainer.appendChild(rankIndicator);
+        thumbnail.appendChild(rankContainer);
+    }
+};
+setInterval(() => {
+    document
+        .querySelectorAll('ul.game-cards.game-tile-list')
+        .forEach((gameList, i) => {
+        gameList
+            .querySelectorAll(`li.list-item.game-card.game-tile:not([${rankAttribute}])`)
+            .forEach((e, i) => {
+            if (e instanceof HTMLElement) {
+                addRankIndicator(e, i);
+            }
+        });
+    });
+    document
+        .querySelectorAll(`div.grid-item-container.game-card-container:not([${rankAttribute}])`)
+        .forEach((e, i) => {
+        if (e instanceof HTMLElement) {
+            addRankIndicator(e, i);
+        }
+    });
+}, 500);
 
 })();
 
